c0248de765965f9f2712dd62dec1f0d5af29d791
Ryutaro YOSHIBA <ryuzee@gmail.com>
2015-10-17 18:14:14 +0900

    update structure

--- generate.rb
+++ generate.rb
@@ -19,7 +19,6 @@ class Card
       :bottom_margin => 0,
       :left_margin => 0,
       :right_margin => 0)
-    @pdf.font_size 12
   end
 
   def save_image(url)
@@ -36,8 +35,11 @@ class Card
   def write(v)
     @pdf.start_new_page unless @page_count == 0
     @page_count += 1
+
     self.save_image(v["image"]) unless File.exist?(self.image_path(v["image"]))
     @pdf.image self.image_path(v["image"]), :width => WIDTH, :height => HEIGHT
+
+    @pdf.font_size 12
     @pdf.fill_color 'cccccc'
     @pdf.transparent(0.5) do
       @pdf.fill_rectangle [0 ,(HEIGHT + TEXT_BOX_HEIGHT)/2], WIDTH, TEXT_BOX_HEIGHT
@@ -46,15 +48,18 @@ class Card
     @pdf.font FONT, :style => :bold do
       @pdf.text_box v["title"], :at => [0, (HEIGHT + TEXT_BOX_HEIGHT) / 2 ], :width => WIDTH, :height => TEXT_BOX_HEIGHT, :align => :center, :valign => :center
     end
+
+    @pdf.font_size 6
+    @pdf.text_box v["url"], :at => [4, 24], :width => WIDTH, :height => TEXT_BOX_HEIGHT, :align => :left, :valign => :center
   end
 
   def save()
-    @pdf.render_file "test.pdf"
+    @pdf.render_file "./build/card.pdf"
   end
 end
 
 card = Card.new()
-data = YAML.load_file('data.yml')
+data = YAML.load_file('./data/data.yml')
 data.each do |v|
   p v["title"]
   card.write(v)
