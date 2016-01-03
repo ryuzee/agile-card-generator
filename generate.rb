37e3fe33afa0802beca9d9ab1021ee7b9a14efb3
Ryutaro YOSHIBA <ryuzee@gmail.com>
2016-01-03 20:17:30 +0900

    use bit.ly

--- generate.rb
+++ generate.rb
@@ -3,6 +3,7 @@ require 'yaml'
 require 'net/http'
 require 'uri'
 require "digest/md5"
+require 'bitly'
 
 FONT = './fonts/japanese_font.ttf'
 
@@ -33,12 +34,29 @@ class Card
     return "./cache/" + Digest::MD5.hexdigest(url) + ".jpg"
   end
 
+  def shorten_data_path(url)
+    return "./cache/" + Digest::MD5.hexdigest(url) + ".txt"
+  end
+
   def write(v)
     @pdf.start_new_page unless @page_count == 0
     @page_count += 1
 
     self.save_image(v["image"]) unless File.exist?(self.image_path(v["image"]))
     @pdf.image self.image_path(v["image"]), :width => WIDTH, :height => HEIGHT
+
+    short_url = ''
+    if File.exist?(self.shorten_data_path(v['image']))
+      File.open self.shorten_data_path(v['image']) do |file|
+        short_url = file.read
+      end
+    else
+      short_url = self.bitly_shorten(v["image"])
+      File.open self.shorten_data_path(v['image']), 'a' do |file|
+        file.write short_url
+      end
+    end
+    puts short_url
 
     @pdf.font_size 14
     @pdf.fill_color 'cccccc'
@@ -51,7 +69,16 @@ class Card
     end
 
     @pdf.font_size 6
-    @pdf.text_box v["url"], :at => [4, 24], :width => WIDTH, :height => TEXT_BOX_HEIGHT, :align => :left, :valign => :center
+    @pdf.text_box short_url, :at => [4, 24], :width => WIDTH, :height => TEXT_BOX_HEIGHT, :align => :left, :valign => :center
+  end
+
+  def bitly_shorten(url)
+    Bitly.use_api_version_3
+    Bitly.configure do |config|
+      config.api_version = 3
+      config.access_token = ENV['BITLY_TOKEN']
+    end
+    Bitly.client.shorten(url).short_url
   end
 
   def save()
@@ -61,6 +88,11 @@ end
 
 if not File.exist?(FONT) then
   puts "You need to put Japanese True Type font in fonts directory with the name `japanese_font.ttf`."
+  exit
+end
+
+unless ENV.has_key?('BITLY_TOKEN')
+  puts "You need to set bitly token as the environmental variable named 'BITLY_TOKEN'"
   exit
 end
 
